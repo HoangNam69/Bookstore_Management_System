0a7cfcfc6b2d1ed15376f65bc9a5b9bb0be3fdbe
Nguyen Duc <ngduc0106@gmail.com>
2024-04-19 20:24:45 +0700

    Test JPA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,7 @@ import jakarta.persistence.Persistence;
  */
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA_ORM_MSSQL");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA_ORM_MARIADB");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
 
