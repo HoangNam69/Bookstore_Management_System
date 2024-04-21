e9393be940c2f802004b998bcb03328d9ecd041a
Ha Huu Vinh <hahuuvinh2003@gmail.com>
2024-04-21 13:38:58 +0700

    update TacGiaDao

--- src/main/java/dao/TacGiaDao.java
+++ src/main/java/dao/TacGiaDao.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 import db.DBConnection;
 import entities.TacGia;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
 
 public class TacGiaDao {
 	private Connection con;
@@ -16,74 +19,72 @@ public class TacGiaDao {
 	private ResultSet rs;
 	private String query;
 	private int rsCheck;
+	private EntityManager em;
 
 	public TacGiaDao() {
-		DBConnection connection = DBConnection.getInstance();
-		con = connection.getConnection();
+		this.em = Persistence.createEntityManagerFactory("JPA_ORM_MARIADB").createEntityManager();
 	}
 
-	public ArrayList<TacGia> getListTacGia() throws Exception {
-		ArrayList<TacGia> list = new ArrayList<>();
-		query = "SELECT maTacGia, tenTacGia FROM TacGia";
-		ps = con.prepareStatement(query);
-		rs = ps.executeQuery();
-		while (rs.next()) {
-			TacGia t = new TacGia(rs.getString("maTacGia"), rs.getString("tenTacGia"));
-			list.add(t);
-		}
-		return list;
+	public List<TacGia> getListTacGia() {
+		TypedQuery<TacGia> query = em.createQuery("SELECT t FROM TacGia t", TacGia.class);
+		return query.getResultList();
 	}
 
 	public List<TacGia> getTacGia(String maTacGia) {
-		List<TacGia> dsTG = new ArrayList<TacGia>();
-		try {
-			String query = "SELECT * FROM TacGia WHERE maTacGia = ?";
-			ps = con.prepareStatement(query);
-			ps.setString(1, maTacGia);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				String matg = rs.getString("maTacGia");
-				String tentg = rs.getString("tenTacGia");
-				TacGia tg = new TacGia(matg, tentg);
-				dsTG.add(tg);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("Error retrieving authors: " + e.getMessage());
-		}
-		return dsTG;
+		TypedQuery<TacGia> query = em.createQuery("SELECT t FROM TacGia t WHERE t.maTacGia = :maTacGia", TacGia.class);
+		query.setParameter("maTacGia", maTacGia);
+		return query.getResultList();
 	}
 
-	public boolean themTacGia(TacGia t) throws Exception {
-		query = "INSERT INTO TacGia (maTacGia, tenTacGia) VALUES (?, ?)";
-		ps = con.prepareStatement(query);
-		ps.setString(1, t.getMaTacGia());
-		ps.setString(2, t.getTenTacGia());
-		rsCheck = ps.executeUpdate();
-		return rsCheck != 0;
+	public boolean themTacGia(TacGia t) {
+		try {
+			em.getTransaction().begin();
+			em.persist(t);
+			em.getTransaction().commit();
+			return true;
+		} catch (Exception e) {
+			if (em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+			e.printStackTrace();
+			System.out.println("Error inserting author: " + e.getMessage());
+			return false;
+		}
 	}
 
 	public boolean xoaTacGia(String maTacGia) {
-		// Your implementation to delete author by ID
-		return false;
+		try {
+			em.getTransaction().begin();
+			TacGia tacGia = em.find(TacGia.class, maTacGia);
+			if (tacGia != null) {
+				em.remove(tacGia);
+				em.getTransaction().commit();
+				return true;
+			}
+			return false; // Không tìm thấy tác giả để xóa
+		} catch (Exception e) {
+			if (em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+			e.printStackTrace();
+			System.out.println("Error deleting author: " + e.getMessage());
+			return false;
+		}
 	}
 
-	public TacGia timTacGia(String TacGia) throws SQLException {
-		query = "SELECT * FROM TacGia WHERE tenTacGia = ?";
-		ps = con.prepareStatement(query);
-		ps.setString(1, TacGia);
-		rs = ps.executeQuery();
-		while (rs.next()) {
-			return new TacGia(rs.getString("maTacGia"), rs.getString("tenTacGia"));
+	public TacGia timTacGia(String tenTacGia) {
+		TypedQuery<TacGia> query = em.createQuery("SELECT t FROM TacGia t WHERE t.tenTacGia = :tenTacGia", TacGia.class);
+		query.setParameter("tenTacGia", tenTacGia);
+		List<TacGia> results = query.getResultList();
+		if (!results.isEmpty()) {
+			return results.get(0);
 		}
 		return null;
 	}
 
-	public boolean kiemTraTonTaiTacGia(String ten) throws SQLException {
-		query = "SELECT * FROM TacGia WHERE tenTacGia = ?";
-		ps = con.prepareStatement(query);
-		ps.setString(1, ten);
-		rs = ps.executeQuery();
-		return rs.next();
+	public boolean kiemTraTonTaiTacGia(String tenTacGia) {
+		TypedQuery<Long> query = em.createQuery("SELECT COUNT(t) FROM TacGia t WHERE t.tenTacGia = :tenTacGia", Long.class);
+		query.setParameter("tenTacGia", tenTacGia);
+		return query.getSingleResult() > 0;
 	}
 }
