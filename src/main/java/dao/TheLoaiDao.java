526dc1e3328038c7bc35c4cb51cf46c7b989111e
Ha Huu Vinh <hahuuvinh2003@gmail.com>
2024-04-21 13:53:37 +0700

    update TheLoaiDao

--- src/main/java/dao/TheLoaiDao.java
+++ src/main/java/dao/TheLoaiDao.java
@@ -10,157 +10,84 @@ import java.util.List;
 import db.DBConnection;
 import entities.TheLoaiSach;
 import entities.TheLoaiVanPhongPham;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
 
 public class TheLoaiDao {
-	private Connection con;
-	private PreparedStatement ps = null;
-	private ResultSet rs;
-	private String query;
-	private int rsCheck;
+    private Connection con;
+    private PreparedStatement ps = null;
+    private ResultSet rs;
+    private String query;
+    private int rsCheck;
+    private EntityManager em;
 
-	public TheLoaiDao() {
-		DBConnection connection = DBConnection.getInstance();
-		con = connection.getConnection();
-	}
+    public TheLoaiDao() {
+        this.em = Persistence.createEntityManagerFactory("JPA_ORM_MARIADB").createEntityManager();
+    }
 
-	public ArrayList<TheLoaiSach> getListTheLoaiSach() {
-		ArrayList<TheLoaiSach> list = new ArrayList<>();
-		try {
-			query = "SELECT maTheLoai, tenTheLoai FROM TheLoaiSach";
-			ps = con.prepareStatement(query);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				TheLoaiSach s = new TheLoaiSach(rs.getString("maTheLoai"), rs.getString("tenTheLoai"));
-				list.add(s);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
+    public List<TheLoaiSach> getListTheLoaiSach() {
+        TypedQuery<TheLoaiSach> query = em.createQuery("SELECT t FROM TheLoaiSach t", TheLoaiSach.class);
+        return query.getResultList();
+    }
 
+    public List<TheLoaiVanPhongPham> getListTheLoaiVanPhongPham() {
+        TypedQuery<TheLoaiVanPhongPham> query = em.createQuery("SELECT t FROM TheLoaiVanPhongPham t", TheLoaiVanPhongPham.class);
+        return query.getResultList();
+    }
 
-	public ArrayList<TheLoaiVanPhongPham> getListTheLoaiVanPhongPham() {
-		ArrayList<TheLoaiVanPhongPham> list = new ArrayList<>();
-		try {
-			query = "SELECT maLoaiVanPhongPham, tenTheLoai FROM LoaiVanPhongPham";
-			ps = con.prepareStatement(query);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				TheLoaiVanPhongPham s = new TheLoaiVanPhongPham(rs.getString("maLoaiVanPhongPham"),
-						rs.getString("tenTheLoai"));
-				list.add(s);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
+    public boolean themTheLoaiSach(TheLoaiSach t) {
+        try {
+            em.getTransaction().begin();
+            em.persist(t);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-
-	public boolean themTheLoaiSach(TheLoaiSach t) {
-		try {
-			query = "INSERT INTO TheLoaiSach (maTheLoai, tenTheLoai) VALUES (?, N'" + t.getTenLoai() + "')";
-			ps = con.prepareStatement(query);
-			ps.setString(1, t.getMaLoai());
-			rsCheck = ps.executeUpdate();
-			if (rsCheck != 0) {
-				return true;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+    public boolean themTheLoaiVanPhongPham(TheLoaiVanPhongPham t) {
+        try {
+            em.getTransaction().begin();
+            em.persist(t);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 
-	public boolean themTheLoaiVanPhongPham(TheLoaiVanPhongPham t) {
-		try {
-			query = "INSERT INTO LoaiVanPhongPham (maLoaiVanPhongPham, tenTheLoai) VALUES (?, N'" + t.getTenLoai() + "')";
-			ps = con.prepareStatement(query);
-			ps.setString(1, t.getMaLoai());
-			rsCheck = ps.executeUpdate();
-			if (rsCheck != 0) {
-				return true;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-
-	public TheLoaiSach timTheLoaiSach(String tenTheLoai) {
-		try {
-			query = "SELECT * FROM TheLoaiSach WHERE tenTheLoai = ?";
-			ps = con.prepareStatement(query);
-			ps.setString(1, tenTheLoai);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				return new TheLoaiSach(rs.getString("maTheLoai"), rs.getString("tenTheLoai"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public TheLoaiSach timTheLoaiSach(String tenTheLoai) {
+        return (TheLoaiSach) em.createNativeQuery("SELECT * FROM TheLoaiSach WHERE tenTheLoai = ?", TheLoaiSach.class).setParameter(1, tenTheLoai).getSingleResult();
+    }
 
 
 	public TheLoaiVanPhongPham timTheLoaiVanPhongPham(String tenTheLoai) {
-		try {
-			query = "SELECT * FROM LoaiVanPhongPham WHERE tenTheLoai = ?";
-			ps = con.prepareStatement(query);
-			ps.setString(1, tenTheLoai);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				return new TheLoaiVanPhongPham(rs.getString("maLoaiVanPhongPham"), rs.getString("tenTheLoai"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
+		return (TheLoaiVanPhongPham) em.createNativeQuery("SELECT * FROM TheLoaiVanPhongPham WHERE tenTheLoai = ?", TheLoaiVanPhongPham.class).setParameter(1, tenTheLoai).getSingleResult();
 	}
-
 
 	public List<TheLoaiSach> getSachTheoTheLoai(String maTL) {
-		List<TheLoaiSach> dsTLS = new ArrayList<>();
-		try {
-			String query = "SELECT * FROM TheLoaiSach WHERE maTheLoai = ?";
-			ps = con.prepareStatement(query);
-			ps.setString(1, maTL);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				String maTheLoai = rs.getString("maTheLoai");
-				String tenTheLoai = rs.getString("tenTheLoai");
-				TheLoaiSach tls = new TheLoaiSach(maTheLoai, tenTheLoai);
-				dsTLS.add(tls);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return dsTLS;
+		return em.createNativeQuery("SELECT * FROM TheLoaiSach WHERE maTheLoai = ?", TheLoaiSach.class).setParameter(1, maTL).getResultList();
 	}
 
-
-	public boolean kiemTraTonTaiTheLoaiVPP(String ten) throws SQLException {
-		query = "SELECT * FROM LoaiVanPhongPham WHERE tenTheLoai = N'" + ten + "'";
-		ps = con.prepareStatement(query);
-		rs = ps.executeQuery();
-		while (rs.next()) {
-			return true;
-		}
-		return false;
+	public boolean kiemTraTonTaiTheLoaiVPP(String ten) {
+		//neu ton tai thi tra ve true
+		return em.createNativeQuery("SELECT COUNT(*) FROM TheLoaiVanPhongPham WHERE tenTheLoai = ?", TheLoaiVanPhongPham.class).setParameter(1, ten).getSingleResult() != null;
 	}
 
-	public boolean kiemTraTonTaiTheLoaiSach(String ten) throws SQLException {
-		query = "SELECT * FROM TheLoaiSach WHERE tenTheLoai = N'" + ten + "'";
-		ps = con.prepareStatement(query);
-		rs = ps.executeQuery();
-		while (rs.next()) {
-			return true;
-		}
-		return false;
+	public boolean kiemTraTonTaiTheLoaiSach(String ten) {
+		//neu ton tai thi tra ve true
+		return em.createNativeQuery("SELECT COUNT(*) FROM TheLoaiSach WHERE tenTheLoai = ?", TheLoaiSach.class).setParameter(1, ten).getSingleResult() != null;
 	}
-
 
 }
