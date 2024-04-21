ccaf4cf4ab8ab672062c9810c855f2af099737fc
Le Hoang Nam <lehoangnam.31123@gmail.com>
2024-04-21 10:47:57 +0700

    Update: CRUD MauSac by Hibernate ORM

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,14 @@
  * Copyright (c) 2024 IUH. All rights reserved.
  */
 
+import dao.MauSacDao;
+import entities.MauSac;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+
+import java.util.ArrayList;
 
 /**
  * @description:
@@ -19,19 +23,45 @@ public class Main {
     public static void main(String[] args) {
 //        JPA_ORM_MARIADB
 //        JPA_ORM_MSSQL
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA_ORM_MARIADB");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA_ORM_MARIADB");
+//        EntityManager em = emf.createEntityManager();
+//        EntityTransaction tx = em.getTransaction();
+//
+//        try {
+//            tx.begin();
+//            tx.commit();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            tx.rollback();
+//        }
+//
+//        em.close();
+//        emf.close();
 
-        try {
-            tx.begin();
-            tx.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            tx.rollback();
+        MauSacDao mauSacDao = new MauSacDao();
+//        ArrayList<MauSac> list = mauSacDao.getListMauSac();
+//        for (MauSac mauSac : list) {
+//            System.out.println(mauSac);
+//        }
+
+//        MauSac mauSac = new MauSac("MS003", "Màu đỏ");
+//        if (mauSacDao.themMauSac(mauSac)) {
+//            System.out.println("Thêm thành công");
+//        } else {
+//            System.out.println("Thêm thất bại");
+//        }
+
+//        MauSac mauSacCapNhat = new MauSac("MS003", "Màu khùng");
+//        if (mauSacDao.capNhatMauSac(mauSacCapNhat)) {
+//            System.out.println("Cập nhật thành công");
+//        } else {
+//            System.out.println("Cập nhật thất bại");
+//        }
+
+        if (mauSacDao.xoaMauSac("MS003s")) {
+            System.out.println("Xóa thành công");
+        } else {
+            System.out.println("Xóa thất bại");
         }
-
-        em.close();
-        emf.close();
     }
 }
--- src/main/java/dao/MauSacDao.java
+++ src/main/java/dao/MauSacDao.java
@@ -8,76 +8,86 @@ import java.util.ArrayList;
 
 import db.DBConnection;
 import entities.MauSac;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 
 public class MauSacDao {
-	private Connection con;
-	private PreparedStatement ps = null;
-	private ResultSet rs;
-	private int rsCheck;
 
-	public MauSacDao() {
-		DBConnection connection = DBConnection.getInstance();
-		con = connection.getConnection();
-	}
+    private EntityManager em;
 
-	public ArrayList<MauSac> getListMauSac() {
-		ArrayList<MauSac> list = new ArrayList<>();
-		String query = "SELECT maMauSac, tenMau FROM MauSac";
-		try (PreparedStatement ps = con.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
-			while (rs.next()) {
-				MauSac mauSac = new MauSac(rs.getString("maMauSac"), rs.getString("tenMau"));
-				list.add(mauSac);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
+    public MauSacDao() {
+        em = Persistence.createEntityManagerFactory("JPA_ORM_MARIADB")
+                .createEntityManager();
+    }
 
-	public boolean themMauSac(MauSac mauSac) {
-		try {
-			if (kiemTraTonTaiMauSac(mauSac.getTenMau())) {
-				System.out.println("Màu sắc đã tồn tại.");
-				return false;
-			}
-			String query = "INSERT INTO MauSac (maMauSac, tenMau) VALUES (?, ?)";
-			try (PreparedStatement ps = con.prepareStatement(query)) {
-				ps.setString(1, mauSac.getMaMau());
-				ps.setString(2, mauSac.getTenMau());
-				rsCheck = ps.executeUpdate();
-				return rsCheck != 0;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+    public ArrayList<MauSac> getListMauSac() {
+        return (ArrayList<MauSac>) em.createNativeQuery("SELECT maMauSac, tenMau FROM MauSac", MauSac.class)
+                .getResultList();
+    }
 
-	public MauSac timMauSac(String mau) {
-		String query = "SELECT * FROM MauSac WHERE tenMau = ?";
-		try (PreparedStatement ps = con.prepareStatement(query)) {
-			ps.setString(1, mau);
-			try (ResultSet rs = ps.executeQuery()) {
-				while (rs.next()) {
-					return new MauSac(rs.getString("maMauSac"), rs.getString("tenMau"));
-				}
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public boolean themMauSac(MauSac mauSac) {
 
-	public boolean kiemTraTonTaiMauSac(String ten) {
-		String query = "SELECT * FROM MauSac WHERE tenMau = ?";
-		try (PreparedStatement ps = con.prepareStatement(query)) {
-			ps.setString(1, ten);
-			try (ResultSet rs = ps.executeQuery()) {
-				return rs.next();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+
+            em.persist(mauSac);
+
+            tx.commit();
+
+            return true;
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public MauSac timMauSac(String mau) {
+        return (MauSac) em.createNativeQuery("SELECT * FROM MauSac WHERE tenMau = ?", MauSac.class)
+                .setParameter(1, mau)
+                .getSingleResult();
+    }
+
+    public boolean kiemTraTonTaiMauSac(MauSac mauSac) {
+        return em.createNativeQuery("SELECT * FROM MauSac WHERE tenMau = ?", MauSac.class)
+                .setParameter(1, mauSac.getTenMau())
+                .getResultList().size() > 0;
+    }
+
+    public boolean capNhatMauSac(MauSac mauSac) {
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+
+            em.merge(mauSac);
+
+            tx.commit();
+
+            return true;
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean xoaMauSac(String maMauSac) {
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            MauSac mauSac = em.find(MauSac.class, maMauSac);
+            em.remove(mauSac);
+
+            tx.commit();
+
+            return true;
+        }catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 }
