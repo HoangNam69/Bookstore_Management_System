e70541a84813c2b192d9192306286bc8d6bd7651
Nguyen Duc <ngduc0106@gmail.com>
2024-04-21 14:06:01 +0700

    Add folder data import

--- src/main/java/dao/ChatLieuDao.java
+++ src/main/java/dao/ChatLieuDao.java
@@ -1,93 +1,55 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
 
-import db.DBConnection;
 import entities.ChatLieu;
 
+import java.util.List;
+
 public class ChatLieuDao {
-	private Connection con;
-	private PreparedStatement ps = null;
-	private ResultSet rs;
-	private String query;
-	private int rsCheck;
+	private EntityManager em;
 
 	public ChatLieuDao() {
-		DBConnection connection = DBConnection.getInstance();
-		con = connection.getConnection();
+		em = Persistence.createEntityManagerFactory("JPA_ORM_MARIADB").createEntityManager();
 	}
 
-	public ArrayList<ChatLieu> getListChatLieu() {
-		ArrayList<ChatLieu> list = new ArrayList<>();
-		query = "SELECT maChatLieu, tenChatLieu FROM ChatLieu";
-		try (PreparedStatement ps = con.prepareStatement(query);
-			 ResultSet rs = ps.executeQuery()) {
-			while (rs.next()) {
-				ChatLieu chatLieu = new ChatLieu(rs.getString("maChatLieu"), rs.getString("tenChatLieu"));
-				list.add(chatLieu);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace(); // Handle the exception more gracefully according to your application's needs
-		}
-		return list;
+	public List<ChatLieu> getListChatLieu() {
+		TypedQuery<ChatLieu> query = em.createQuery("SELECT c FROM ChatLieu c", ChatLieu.class);
+		return query.getResultList();
 	}
 
 	public boolean themChatLieu(ChatLieu l) {
-		query = "INSERT INTO ChatLieu (maChatLieu, tenChatLieu) VALUES (?, ?)";
-		try (PreparedStatement ps = con.prepareStatement(query)) {
-			ps.setString(1, l.getMaChatLieu());
-			ps.setString(2, l.getTenChatLieu());
-			rsCheck = ps.executeUpdate();
-			return rsCheck != 0;
-		} catch (SQLException e) {
-			e.printStackTrace(); // Handle the exception more gracefully according to your application's needs
+		EntityTransaction tx = em.getTransaction();
+		try {
+			tx.begin();
+			em.persist(l);
+			tx.commit();
+			return true;
+		} catch (Exception e) {
+			if (tx.isActive()) tx.rollback();
+			e.printStackTrace();
 			return false;
 		}
 	}
 
-
-	public ChatLieu timChatLieu(String tenChatLieu) throws SQLException {
-		query = "select * from ChatLieu where tenChatLieu = ?";
-		ps = con.prepareStatement(query);
-		ps.setString(1, tenChatLieu);
-		rs = ps.executeQuery();
-		while (rs.next()) {
-			return new ChatLieu(rs.getString("maChatLieu"), rs.getString("tenChatLieu"));
-		}
-		return null;
+	public ChatLieu timChatLieu(String tenChatLieu) {
+		TypedQuery<ChatLieu> query = em.createQuery("SELECT c FROM ChatLieu c WHERE c.tenChatLieu = :tenChatLieu", ChatLieu.class);
+		query.setParameter("tenChatLieu", tenChatLieu);
+		return query.getSingleResult();
 	}
 
 	public List<ChatLieu> getChatLieu(String maChatLieu) {
-		List<ChatLieu> dsCL = new ArrayList<ChatLieu>();
-		try {
-			String query = "Select * from ChatLieu where maChatLieu = ?";
-			ps = con.prepareStatement(query);
-			ps.setString(1, maChatLieu);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				String macl = rs.getString("maChatLieu");
-				String tencl = rs.getString("tenChatLieu");
-				ChatLieu cl = new ChatLieu(macl, tencl);
-				dsCL.add(cl);
-			}
-		} catch (SQLException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-		return dsCL;
+		TypedQuery<ChatLieu> query = em.createQuery("SELECT c FROM ChatLieu c WHERE c.maChatLieu = :maChatLieu", ChatLieu.class);
+		query.setParameter("maChatLieu", maChatLieu);
+		return query.getResultList();
 	}
-	public boolean kiemTraTonTaiChatLieu(String ten) throws SQLException {
-		query = "select * from ChatLieu where tenChatLieu = N'"+ten+"'";
-		ps = con.prepareStatement(query);
-		rs = ps.executeQuery();
-		while (rs.next()) {
-			return true;
-		}
-		return false;
+
+	public boolean kiemTraTonTaiChatLieu(String ten) {
+		TypedQuery<ChatLieu> query = em.createQuery("SELECT c FROM ChatLieu c WHERE c.tenChatLieu = :tenChatLieu", ChatLieu.class);
+		query.setParameter("tenChatLieu", ten);
+		return !query.getResultList().isEmpty();
 	}
 }
