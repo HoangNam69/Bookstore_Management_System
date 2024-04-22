bd2d49ccc12071b45c89445b81688700caddd660
Le Hoang Nam <lehoangnam.31123@gmail.com>
2024-04-22 20:28:28 +0700

    Add:create util package - Constants save ipv4 and port

---
+++ src/main/java/util/Constants.java
@@ -0,0 +1,17 @@
+/**
+ * @ (#) Constants.java 1.0 22-Apr-24
+ * <p>
+ * Copyright (c) 2024 IUH. All rights reserved.
+ */
+package util;
+
+/**
+ * @description:
+ * @author: Le Hoang Nam
+ * @date: 22-Apr-24
+ * @version: 1.0
+ */
+public final class Constants {
+    public static final String ipv4 = "172.20.34.194";
+    public static final String port = "7878";
+}
